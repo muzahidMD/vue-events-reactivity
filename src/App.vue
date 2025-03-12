635b4ca0b70279227102da644a6e186f5af697f4
MD: Muzahidul Islam <75301375+muzahidMD@users.noreply.github.com>
2025-03-12 09:42:51 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,7 @@ function clickMe() {
             </div>
             <!-- Content -->
             <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex == 3">
-              <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 3 ? '' : 'hidden'">
+            <!-- <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 3 ? '' : 'hidden'"> -->
                 <p class="leading-6 font-light pl-9">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui
                   architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum
