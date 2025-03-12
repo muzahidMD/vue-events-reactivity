014adf098eb1e92f7a72ddd20e5d3885ad62e7b3
Muzahidul Islam <mujahidul.core@gmail.com>
2025-03-12 11:07:25 +0600

    reactive array

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref } from "vue";
-
-const accordionIndex = ref(0)
-function clickMe() {
-  alert('ClickME');
+import { reactive, ref } from "vue";
+const accordions = reactive([
+  false, false, false, false
+]);
+function toggle(index) {
+  accordions[index] = !accordions[index]
 }
 </script>
 
@@ -25,14 +26,14 @@ function clickMe() {
           <!-- What is term -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex = 0"
+            <!-- <div @click="toggle(0)" -->
+            <div @click="accordions[0] = !accordions[0]"
               class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
               <i class="fas fa-plus"></i>
               <h3>What is term?</h3>
             </div>
             <!-- Content -->
-            <!-- <div class="px-5 pt-0 text-left pb-5" :class="accordionIndex == 0 ? '' : 'hidden'"> -->
-            <div class="px-5 pt-0 text-left pb-5" v-show="accordionIndex == 0">
+            <div class="px-5 pt-0 text-left pb-5" v-show="accordions[0]">
               <p class="leading-6 font-light pl-9 ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolor dolorum odio, saepe quibusdam
                 iusto possimus nesciunt dolores assumenda quae totam, doloremque odit. Itaque cum animi, labore debitis
@@ -44,14 +45,13 @@ function clickMe() {
           <!-- When to use Accordion Components -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex = 1"
+            <div @click="toggle(1)"
               class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
               <i class="fas fa-plus"></i>
               <h3>When to use Accordion Components?</h3>
             </div>
             <!-- Content -->
-            <!-- <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 1 ? '' : 'hidden'"> -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex == 1">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[1]">
               <p class="leading-6 font-light pl-9 ">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit itaque, sequi incidunt ab sit
                 optio debitis dignissimos doloribus ratione nobis id mollitia maiores eveniet necessitatibus dolorum
@@ -63,14 +63,13 @@ function clickMe() {
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex = 2"
+            <div @click="toggle(2)"
               class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
               <i class="fas fa-plus"></i>
               <h3>How can it be defined?</h3>
             </div>
             <!-- Content -->
-            <!-- <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 2 ? '' : 'hidden'"> -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex == 2">
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[2]">
               <p class="leading-6 font-light pl-9">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facilis labore voluptates provident nam,
                 delectus suscipit nihil optio voluptate quae porro fugiat magni excepturi doloribus.
@@ -81,20 +80,18 @@ function clickMe() {
           <!-- Accordion Wrapper -->
           <div class="transition hover:bg-indigo-50">
             <!-- header -->
-            <div @click="accordionIndex = 3"
+            <div @click="toggle(3)"
               class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
               <i class="fas fa-plus"></i>
               <h3>Chamber reached do he nothing be?</h3>
             </div>
             <!-- Content -->
-            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordionIndex == 3">
-              <div class="accordion-content px-5 pt-0 text-left pb-5" :class="accordionIndex == 3 ? '' : 'hidden'">
-                <p class="leading-6 font-light pl-9">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui
-                  architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum
-                  ad
-                </p>
-              </div>
+            <div class="accordion-content px-5 pt-0 text-left pb-5" v-show="accordions[3]">
+              <p class="leading-6 font-light pl-9">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui
+                architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum
+                ad
+              </p>
             </div>
           </div>
         </div>
